0392664df673e2089dee37f797eb4bc19c7a5569
wp2novaediasti <nofaediasti@gmail.com>
2021-10-15 01:11:13 +0700

    membuat template web sederhana web.php

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan    : Nova Ediasti</li>
+        <li>Nama Belakang : Romadhoni</li>
+        <li>Nama Lengkap  : Nova Ediasti Romadhoni</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> Jalan yang pernah ada dimana mana</li>
+</u>
+
+<h4>Tempat Tanggal Lahir</h4>
+<ul type="none">
+    <li>Yogyakarta, 16 November 2001</li>
+</ul>
+
+<h4>Olahrga Favorit</h4>
+<ul type="square">
+    <li>Voli</li>
+    <li>Basket</li>
+</ul>
+</section>
